00fbf78eec6432b47f3b7bc13e36082ac747888e
Salman Hashmi <salman.hashmi2@mail.mcgill.ca>
2016-10-24 04:12:11 -0400

    add C source file vigenere_cipher

---
+++ vigenere_cipher.c
@@ -0,0 +1,6 @@
+/**
+ * Takes a key and a string plaintext as command-line arguments, encrypts the plaintext 
+ * using Vigenère’s cipher and reports the resulting ciphertext via printf.
+ *
+ * @author  salman hashmi
+ */  
