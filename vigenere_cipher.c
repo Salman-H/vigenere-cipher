ed78a4ba9b9f27809ee41d929ba5c11b5413a7f7
Salman Hashmi <salman.hashmi2@mail.mcgill.ca>
2016-10-24 04:26:04 -0400

    add required C libraries

--- vigenere_cipher.c
+++ vigenere_cipher.c
@@ -2,5 +2,9 @@
  * Takes a key and a string plaintext as command-line arguments, encrypts the plaintext 
  * using Vigenère’s cipher and reports the resulting ciphertext via printf.
  *
- * @author  salman hashmi
+ * @author salman hashmi
  */  
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
